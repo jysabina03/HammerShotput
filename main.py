b46f33f3d7b6c5935590aa20b921e6c13c07645c
jysabina03 <jysabina03@tukorea.ac.kr>
2023-12-05 02:57:47 +0900

    타이틀 모드 구현

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from pico2d import open_canvas, close_canvas
 
 import game_framework
 import play_mode
-
+import title_mode
 
 open_canvas()
-game_framework.run(play_mode)
+game_framework.run(title_mode)
 close_canvas()
 
--- title_mode.py
+++ title_mode.py
@@ -1,22 +1,37 @@
 from pico2d import load_image, get_events, clear_canvas, update_canvas, get_time, get_canvas_height
-from sdl2 import SDLK_ESCAPE, SDL_KEYDOWN, SDL_QUIT, SDLK_SPACE, SDL_MOUSEMOTION
+from sdl2 import SDLK_ESCAPE, SDL_KEYDOWN, SDL_QUIT, SDLK_SPACE, SDL_MOUSEMOTION, SDL_MOUSEBUTTONDOWN
 
 import game_framework
 import play_mode
 
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION_FAST = 12
+FRAMES_PER_ACTION_SLOW = 0.5
+
 
 def init():
-    global BG_image, grass1,button_play,button_how
-    global mouse_on_button
+    global BG_image, grass1, button_play, button_how, image_kirby, image_DDD
+    global button_onoff
 
-    global buttonX,buttonY
-    buttonX=400
-    buttonY=(200,140)
+    global buttonX, buttonY
+    global cur_mouse_x, cur_mouse_y, frame
+
+    frame = 0
+
+    image_kirby = load_image('./texture/sp_kirby.png')
+    image_DDD = load_image('./texture/sp_dedede.png')
+
+    buttonX = 400
+    buttonY = (240, 140)
+    cur_mouse_x, cur_mouse_y = 0, 0
+
     BG_image = load_image('./texture/Title_background.png')
     grass1 = load_image('./texture/grass.png')
-    button_play = (load_image('./texture/Title_button_play_off.png'), load_image('./texture/Title_button_play_on.png'))
-    button_how = (load_image('./texture/Title_button_how_off.png'), load_image('./texture/Title_button_how_on.png'))
+    button_play = (load_image('./texture/Title_button_play_on.png'), load_image('./texture/Title_button_play_off.png'))
+    button_how = (load_image('./texture/Title_button_how_on.png'), load_image('./texture/Title_button_how_off.png'))
 
+    button_onoff = {'play': False, 'how': False}
     pass
 
 
@@ -25,10 +40,7 @@ def finish():
 
 
 def handle_events():
-
     global cur_mouse_x, cur_mouse_y
-
-
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -37,23 +49,55 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_MOUSEMOTION:  # 마우스 움직임 감지
             cur_mouse_x, cur_mouse_y = event.x, get_canvas_height() - 1 - event.y  # y 좌표 뒤집음
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if button_onoff['play']:
+                game_framework.change_mode(play_mode)
+            elif button_onoff['how']:
+                pass
 
     pass
 
 
 def update():
-    if
+    if buttonX + 130 > cur_mouse_x > buttonX - 130 and cur_mouse_y < buttonY[0] + 37 and cur_mouse_y > \
+            buttonY[0] - 37:
+        button_onoff['play'] = True
+    else:
+        button_onoff['play'] = False
+
+    if buttonX + 130 > cur_mouse_x > buttonX - 130 and cur_mouse_y < buttonY[1] + 37 and cur_mouse_y > \
+            buttonY[1] - 37:
+        button_onoff['how'] = True
+    else:
+        button_onoff['how'] = False
+
     pass
 
 
 def draw():
+    global frame
     clear_canvas()
     BG_image.draw(400, 300)
-    grass1.draw(400, 300)
-    if cur_mouse_x<buttonX+130 and cur_mouse_x>buttonX-130 and cur_mouse_y<buttonY[0]+37and cur_mouse_y > buttonY[0]-37:
-        button_play[0].draw(buttonX,buttonY)
+    grass1.draw(400, 30)
+
+    frame = (frame + FRAMES_PER_ACTION_SLOW * ACTION_PER_TIME * game_framework.frame_time) % 2
+
+    if button_onoff['play']:
+        button_play[0].draw(buttonX, buttonY[0])
     else:
-        button_play[1].draw(buttonX,buttonY)
+        button_play[1].draw(buttonX, buttonY[0])
+
+    if button_onoff['how']:
+        button_how[0].draw(buttonX, buttonY[1])
+    else:
+        button_how[1].draw(buttonX, buttonY[1])
+
+    image_kirby.clip_composite_draw(int(frame) * 100, 5 * 100, 100, 100, 0, '', 100,
+                                    95,
+                                    100 * 2, 100 * 2)
+    image_DDD.clip_composite_draw(int(frame) * 100, 5 * 100, 100, 100, 0, 'h', 680,
+                                  95 + 45,
+                                  100 * 2, 100 * 2)
 
     update_canvas()
     pass
